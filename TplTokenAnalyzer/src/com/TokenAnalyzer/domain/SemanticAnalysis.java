fb6c89440eb7eb8441039f9b0299e12a537d47c9
Hans Rubenecia <Hans Rubenecia@DESKTOP-F3F989R>
2022-12-13 18:24:23 +0800

    Third Commit

--- TplTokenAnalyzer/src/com/TokenAnalyzer/domain/SemanticAnalysis.java
+++ TplTokenAnalyzer/src/com/TokenAnalyzer/domain/SemanticAnalysis.java
@@ -36,7 +36,6 @@ public class SemanticAnalysis
 		{
 			return "Semantically Incorrect!";
 		}
-		
 
 	}
 }
--- TplTokenAnalyzer/src/com/TokenAnalyzer/domain/SyntaxAnalysis.java
+++ TplTokenAnalyzer/src/com/TokenAnalyzer/domain/SyntaxAnalysis.java
@@ -1,20 +1,12 @@
 package com.TokenAnalyzer.domain;
 
-import java.util.HashMap;
-
 public class SyntaxAnalysis 
 {
 	public String Analysis(String input) 
 	{
 		
 		String[] inputSplit = input.split(">");
-		HashMap<String, Integer> hm = new HashMap<String, Integer>();
 		String output = "";
-		hm.put("<data_type", 1);
-		hm.put("<identifier", 2);
-		hm.put("<assignment_operator", 3);
-		hm.put("<value", 4);
-		hm.put("<delimiter", 5);
 		
 		for (String a : inputSplit)
 		{
